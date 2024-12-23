6c85b682b7cb1bdd2fcde3758713cbcbc7dfea94
sarfraz-hafsa <hafsasarfraz509@gmail.com>
2024-12-24 00:17:37 +0500

    Add files via upload
    constants

---
+++ constants.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 700
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (200, 200, 200)
+BLUE = (0, 51, 204)
+RED = (255, 0, 0)
+YELLOW = (251, 155, 0)
+LIGHT = (255, 232, 178)
+
+# Fonts
+pygame.font.init()
+FONT = pygame.font.Font(None, 40)
+BIG_FONT = pygame.font.Font(None, 60)
+SMALL_FONT = pygame.font.Font(None, 30)
